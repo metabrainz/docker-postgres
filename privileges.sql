eb72e5f956cb46512d10f8a288f50013bc184bdd
Roman Tsukanov <roman@tsukanov.me>
2016-10-21 17:35:43 +0200

    Update privileges

--- privileges.sql
+++ privileges.sql
@@ -4,6 +4,15 @@ BEGIN;
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA musicbrainz FROM musicbrainz_ro;
 REVOKE ALL PRIVILEGES ON SCHEMA musicbrainz FROM musicbrainz_ro;
 
+GRANT CONNECT ON DATABASE musicbrainz_db TO musicbrainz_ro;
+GRANT USAGE ON SCHEMA musicbrainz TO musicbrainz_ro;
 GRANT SELECT ON ALL TABLES IN SCHEMA musicbrainz TO musicbrainz_ro;
+
+-- CAA redirect
+GRANT CONNECT ON DATABASE musicbrainz_db TO caa_redirect;
+GRANT USAGE ON SCHEMA musicbrainz TO caa_redirect;
+GRANT SELECT ON ALL TABLES IN SCHEMA musicbrainz TO caa_redirect;
+GRANT USAGE ON SCHEMA cover_art_archive TO caa_redirect;
+GRANT SELECT ON ALL TABLES IN SCHEMA cover_art_archive TO caa_redirect;
 
 COMMIT;
