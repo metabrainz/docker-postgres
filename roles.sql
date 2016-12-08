1fc11e57b5979dc6db1b65d3bf3c7c3ac2f5ad17
Michael Wiencek <mwtuea@gmail.com>
2016-12-08 14:52:09 -0600

    Sort

--- roles.sql
+++ roles.sql
@@ -1,12 +1,12 @@
 \set ON_ERROR_STOP 1
 BEGIN;
 
-CREATE ROLE replication WITH LOGIN REPLICATION;
-CREATE ROLE musicbrainz_ro;
-CREATE ROLE metabrainz WITH PASSWORD 'metabrainz';
 CREATE ROLE caa_redirect WITH PASSWORD 'caa_redirect';
 CREATE ROLE critiquebrainz WITH PASSWORD 'critiquebrainz';
 CREATE ROLE mbspotify WITH PASSWORD 'mbspotify';
+CREATE ROLE metabrainz WITH PASSWORD 'metabrainz';
+CREATE ROLE musicbrainz_ro;
+CREATE ROLE replication WITH LOGIN REPLICATION;
 CREATE ROLE sentry WITH PASSWORD 'sentry';
 
 COMMIT;
