cb1a202c4c427facd63f988c9d873d802c5c2f2b
Roman Tsukanov <roman@tsukanov.me>
2017-04-27 19:56:23 +0200

    Add a database for test instance of the MeB website

--- roles.sql
+++ roles.sql
@@ -5,6 +5,7 @@ CREATE ROLE caa_redirect WITH PASSWORD 'caa_redirect';
 CREATE ROLE critiquebrainz WITH PASSWORD 'critiquebrainz';
 CREATE ROLE mbspotify WITH PASSWORD 'mbspotify';
 CREATE ROLE metabrainz WITH PASSWORD 'metabrainz';
+CREATE ROLE metabrainz_test WITH PASSWORD 'metabrainz_test';
 CREATE ROLE musicbrainz_ro;
 CREATE ROLE replication WITH LOGIN REPLICATION;
 CREATE ROLE sentry WITH PASSWORD 'sentry';
