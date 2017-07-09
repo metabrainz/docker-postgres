eaea171226f7885fcc6ad97d91e4af8f21b736b0
Roman Tsukanov <roman@tsukanov.me>
2017-07-09 19:21:14 +0200

    Set search_path for the musicbrainz_ro user

--- roles.sql
+++ roles.sql
@@ -11,6 +11,8 @@ CREATE ROLE replication WITH LOGIN REPLICATION;
 CREATE ROLE sentry WITH PASSWORD 'sentry';
 CREATE ROLE telegraf WITH LOGIN;
 CREATE ROLE sir WITH LOGIN PASSWORD 'sir';
+
+ALTER ROLE musicbrainz_ro SET search_path TO musicbrainz, public;
 ALTER ROLE sir SET search_path TO musicbrainz, public;
 
 COMMIT;
